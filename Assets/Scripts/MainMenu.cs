604edab85fcf82af72c8f96e7d014a36ce8df5c9
Kirin Tang <90722097+kirinand@users.noreply.github.com>
2024-02-25 22:07:38 -0800

    added a rule panel

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _sideSelectionMenu;
+    [SerializeField] private GameObject _rulePanel;
 
     public void PlayGame(int ai) // 0 for white, 1 for black, -1 if no AI
     {   
@@ -13,14 +14,14 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void OpenSideSelectionMenu()
+    public void ToggleSideSelectionMenu(bool state)
     {
-        _sideSelectionMenu.SetActive(true);
+        _sideSelectionMenu.SetActive(state);
     }
 
-    public void CloseSideSelectionMenu() 
+    public void ToggleRulePanel(bool state)
     {
-        _sideSelectionMenu.SetActive(false);
+        _rulePanel.SetActive(state);
     }
 
     public void QuitGame()
